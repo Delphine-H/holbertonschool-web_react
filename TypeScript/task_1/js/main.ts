193ac25c23d586f397f88a11f5499831e8be5da6
Delphine HANNON <hannon.delphine@gmail.com>
2024-09-25 14:48:53 +0200

    Add interface Teacher

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,22 @@
+// task_1/js/main.ts
+
+// Définir l'interface Teacher
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+// Exemple d'utilisation de l'interface Teacher
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
